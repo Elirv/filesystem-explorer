a703ba85cb54e708adde9f2fc3ed4a8375b16ed0
tonymg1 <tonymartingaldeano@gmail.com>
2022-09-11 20:10:12 +0200

    try pring img

--- datosImg.php
+++ datosImg.php
@@ -4,13 +4,15 @@ session_start();
 $nombreImg = $_FILES["fileToUpload"]["name"];
 $tipoImg = $_FILES["fileToUpload"]["type"];
 $imgSize = $_FILES["fileToUpload"]["size"];
+$carpertaDestino = $_SERVER['DOCUMENT_ROOT']."/server_variables/";
+$imgTemp = move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$carpertaDestino.$nombreImg);
 
-echo $_FILES["fileToUpload"]["size"];
+
+if($imgTemp){
+    echo "subido exitosamente<br>";
+   
+}
 
 
-$carpertaDestino = $_SERVER['DOCUMENT_ROOT']."/server_variables";
-move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$carpertaDestino.$nombreImg);
-
-echo "hola"
 ?>
 
