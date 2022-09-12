81310801ae74b0894dd08bdd482b0c0c18b8c315
tonymg1 <tonymartingaldeano@gmail.com>
2022-09-12 10:03:26 +0200

    upload img

--- datosImg.php
+++ datosImg.php
@@ -1,16 +1,20 @@
 <?php
 session_start();
 //Receive image data
-$nombreImg = $_FILES["fileToUpload"]["name"];
-$tipoImg = $_FILES["fileToUpload"]["type"];
-$imgSize = $_FILES["fileToUpload"]["size"];
-$carpertaDestino = $_SERVER['DOCUMENT_ROOT']."/server_variables/";
-$imgTemp = move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$carpertaDestino.$nombreImg);
+$archivo = $_FILES['fileToUpload'] ['tmp_name'];
+$nombre = $_FILES['fileToUpload'] ['name'];
+$tipo = $_FILES['fileToUpload'] ['type'];
+$tamano = $_FILES['fileToUpload'] ['size'];
 
+$destino = "ficheros/" . $nombre;
 
-if($imgTemp){
-    echo "subido exitosamente<br>";
-   
+$subido = move_uploaded_file($archivo, $destino);
+
+if($subido){
+    echo "subido exitosamente <br/>";
+    echo "<img src='ficheros/" . $nombre ."'>";
+}else{
+    echo "no suido";
 }
 
 
