46bc19d5a5e21e378fdb23a885845ef65af8659c
tonymg1 <tonymartingaldeano@gmail.com>
2022-09-13 17:20:44 +0200

    revisando delete

--- ficheros/delete.php
+++ ficheros/delete.php
@@ -25,9 +25,9 @@
     </form>
     <?php
 if(isset($_POST["deleteNew"])){
-    if ($_POST["delete"] =="File"){
+    if ($_POST["delete"] !="File"){
        
-        header("Location: crear-directorio.php");
+        header("Location: eliminarDirectorio.php");
     } else{
         header("Location: eliminarDirectorio.php");
     }
